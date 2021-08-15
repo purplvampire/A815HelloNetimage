aecf711aadb55618fd57c3b9c1ccf19a2c32b901
Jason Chen <purplvampire@gmail.com>
2021-08-15 14:00:27 +0800

    modify

--- A815HelloNetimage/ViewController.swift
+++ A815HelloNetimage/ViewController.swift
@@ -9,10 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theImageView: UIImageView!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+            super.viewDidLoad()
+            let start = Date().timeIntervalSince1970
+            let imageAddress = "https://shop.r10s.jp/aikimania/cabinet/new/sw-1758_sp.jpg"
+            if let imageURL = URL(string: imageAddress){
+                DispatchQueue.global().async {
+                    do{
+                        let imageData = try Data(contentsOf: imageURL)
+                        DispatchQueue.main.async {
+                            self.theImageView.image = UIImage(data: imageData)
+                            print("loded:\(Date().timeIntervalSince1970 - start)")
+                        }
+                    }catch{
+                        print(error.localizedDescription)
+                    }
+                }
+                print("next:\(Date().timeIntervalSince1970 - start)")
+            }
+        }
 
 
 }
